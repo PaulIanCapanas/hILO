c46a44203852aca12c9a5fcc7fbd64f8bfb4bf3b
itchydimeee <124387580+itchydimeee@users.noreply.github.com>
2024-10-25 11:32:36 +0800

    [fix] fix some errors

--- components/GoogleSignIn.tsx
+++ components/GoogleSignIn.tsx
@@ -1,28 +1,52 @@
-import React from 'react'
-import { Button } from 'react-native'
-import {
-  GoogleAuthProvider,
-  signInWithRedirect,
-  getRedirectResult
-} from 'firebase/auth'
-import { auth } from '../firebase/initializeFirebase'
+import React from "react";
+import { Button } from "react-native";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithCredential, GoogleAuthProvider } from "firebase/auth";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
 
-export default function GoogleSignIn () {
-  const provider = new GoogleAuthProvider()
-  const handleGoogleSignIn = async () => {
-    try {
-      await signInWithRedirect(auth, provider)
+WebBrowser.maybeCompleteAuthSession();
 
-      const result = await getRedirectResult(auth)
-      if (result) {
-        const credential = GoogleAuthProvider.credentialFromResult(result)
-        const token = credential?.accessToken
-        const user = result.user
-        console.log('User info:', user)
-      }
-    } catch (error) {
-      console.error('Google Sign-In Error:', error)
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default function GoogleSignIn() {
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    clientId: "644044288240-fvg1sfjskh5ih21a935f990ati6dvln1.apps.googleusercontent.com",
+  });
+
+  React.useEffect(() => {
+    if (response?.type === "success") {
+      const { id_token } = response.params;
+      const credential = GoogleAuthProvider.credential(id_token);
+      signInWithCredential(auth, credential)
+        .then((userCredential) => {
+          console.log("User signed in: ", userCredential.user);
+        })
+        .catch((error) => {
+          console.error("Google Sign-In Error: ", error);
+        });
     }
-  }
-  return <Button title='Sign in with Google' onPress={handleGoogleSignIn} />
+  }, [response]);
+
+  return (
+    <Button
+      disabled={!request}
+      title="Sign in with Google"
+      onPress={() => {
+        promptAsync();
+      }}
+    />
+  );
 }
