dbd02f95a2d676f69ed14bb63e33320d113377b6
itchydimeee <124387580+itchydimeee@users.noreply.github.com>
2024-10-25 22:10:36 +0800

    [Fix] fixed google sign in errors

--- components/GoogleSignIn.tsx
+++ components/GoogleSignIn.tsx
@@ -1,25 +1,12 @@
 import React from "react";
 import { Button } from "react-native";
-import { initializeApp } from "firebase/app";
-import { getAuth, signInWithCredential, GoogleAuthProvider } from "firebase/auth";
+import { signInWithCredential, GoogleAuthProvider } from "firebase/auth";
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
+import { auth } from "@/firebase/initializeFirebase";
+
 
 WebBrowser.maybeCompleteAuthSession();
-
-const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
 export default function GoogleSignIn() {
   const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
