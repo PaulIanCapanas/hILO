284ed2fc638a802576e4a88c5d0cce06a91e9f61
itchydimeee <124387580+itchydimeee@users.noreply.github.com>
2024-10-31 22:07:35 +0800

    [refactor] minor  changes

--- components/GoogleSignIn.tsx
+++ components/GoogleSignIn.tsx
@@ -1,50 +1,47 @@
-import React from 'react'
-import { Button } from 'react-native'
-import { signInWithCredential, GoogleAuthProvider } from 'firebase/auth'
-import * as WebBrowser from 'expo-web-browser'
-import * as Google from 'expo-auth-session/providers/google'
-import { auth } from '@/firebase/initializeFirebase'
-import { makeRedirectUri } from 'expo-auth-session'
+import React, { useEffect } from 'react';
+import { Button } from 'react-native';
+import { 
+  signInWithCredential, 
+  GoogleAuthProvider 
+} from 'firebase/auth';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import { auth } from '@/firebase/initializeFirebase'; 
+import { makeRedirectUri } from 'expo-auth-session';
 
-WebBrowser.maybeCompleteAuthSession() 
 
-export default function GoogleSignIn () {
+WebBrowser.maybeCompleteAuthSession();
+
+export default function GoogleSignInButton() {
+
   const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-    clientId: process.env.EXPO_PUBLIC_EXPO_ID,
-    webClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID_WEB,
+    clientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID_WEB,
     androidClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID_ANDROID,
     responseType: 'id_token',
     redirectUri: makeRedirectUri({
-      scheme: 'myapp'
-    })
-  })
+      scheme: 'hilo-app' 
+    }),
+  });
 
-  React.useEffect(() => {
-    console.log(
-      makeRedirectUri({
-        scheme: 'myapp'
-      })
-    )
+  useEffect(() => {
     if (response?.type === 'success') {
-      const { id_token } = response.params
-      const credential = GoogleAuthProvider.credential(id_token)
+      const { id_token } = response.params;
+      const credential = GoogleAuthProvider.credential(id_token);
       signInWithCredential(auth, credential)
-        .then(userCredential => {
-          console.log('User signed in: ', userCredential.user)
+        .then((userCredential) => {
+          console.log('User signed in:', userCredential.user);
         })
-        .catch(error => {
-          console.error('Google Sign-In Error: ', error)
-        })
+        .catch((error) => {
+          console.error('Firebase Sign-In Error: ', error);
+        });
     }
-  }, [response])
+  }, [response]);
 
   return (
-    <Button
-      disabled={!request}
-      title='Sign in with Google'
-      onPress={() => {
-        promptAsync()
-      }}
+    <Button 
+      disabled={!request} 
+      title="Sign in with Google" 
+      onPress={() => promptAsync()} 
     />
-  )
-}
+  );
+};
