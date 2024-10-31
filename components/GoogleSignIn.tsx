825a33ff5ee6829181211453f50eac0a6cff194b
itchydimeee <124387580+itchydimeee@users.noreply.github.com>
2024-10-31 18:07:19 +0800

    [fix] minor changes on google sign in

--- components/GoogleSignIn.tsx
+++ components/GoogleSignIn.tsx
@@ -1,39 +1,50 @@
-import React from "react";
-import { Button } from "react-native";
-import { signInWithCredential, GoogleAuthProvider } from "firebase/auth";
-import * as WebBrowser from "expo-web-browser";
-import * as Google from "expo-auth-session/providers/google";
-import { auth } from "@/firebase/initializeFirebase";
+import React from 'react'
+import { Button } from 'react-native'
+import { signInWithCredential, GoogleAuthProvider } from 'firebase/auth'
+import * as WebBrowser from 'expo-web-browser'
+import * as Google from 'expo-auth-session/providers/google'
+import { auth } from '@/firebase/initializeFirebase'
+import { makeRedirectUri } from 'expo-auth-session'
 
+WebBrowser.maybeCompleteAuthSession() 
 
-WebBrowser.maybeCompleteAuthSession();
-
-export default function GoogleSignIn() {
+export default function GoogleSignIn () {
   const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-    clientId: process.env.EXPO_PUBLIC_CLIENT_ID,
-  });
+    clientId: process.env.EXPO_PUBLIC_EXPO_ID,
+    webClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID_WEB,
+    androidClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID_ANDROID,
+    responseType: 'id_token',
+    redirectUri: makeRedirectUri({
+      scheme: 'myapp'
+    })
+  })
 
   React.useEffect(() => {
-    if (response?.type === "success") {
-      const { id_token } = response.params;
-      const credential = GoogleAuthProvider.credential(id_token);
+    console.log(
+      makeRedirectUri({
+        scheme: 'myapp'
+      })
+    )
+    if (response?.type === 'success') {
+      const { id_token } = response.params
+      const credential = GoogleAuthProvider.credential(id_token)
       signInWithCredential(auth, credential)
-        .then((userCredential) => {
-          console.log("User signed in: ", userCredential.user);
+        .then(userCredential => {
+          console.log('User signed in: ', userCredential.user)
         })
-        .catch((error) => {
-          console.error("Google Sign-In Error: ", error);
-        });
+        .catch(error => {
+          console.error('Google Sign-In Error: ', error)
+        })
     }
-  }, [response]);
+  }, [response])
 
   return (
     <Button
       disabled={!request}
-      title="Sign in with Google"
+      title='Sign in with Google'
       onPress={() => {
-        promptAsync();
+        promptAsync()
       }}
     />
-  );
+  )
 }
