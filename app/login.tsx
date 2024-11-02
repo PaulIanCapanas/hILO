faebf70650c4deb4953fe25a483d72a9deb5ffae
caspted <teddiejohn.rajeev26@gmail.com>
2024-11-02 21:49:40 +0800

    [Optimization] of login screens

--- app/login.tsx
+++ app/login.tsx
@@ -1,39 +1,69 @@
-import { View, Text, Button, TextInput, Alert, StyleSheet} from "react-native"
-import { auth } from "@/firebase/initializeFirebase"
-import { useState } from "react"
-import { signInWithEmailAndPassword } from "firebase/auth"
-import GoogleSignIn from "@/components/GoogleSignIn"
+import { View, Text, TextInput, Alert, TouchableOpacity } from "react-native";
+import { auth } from "@/firebase/initializeFirebase";
+import { useState, useMemo } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import GoogleSignIn from "@/components/GoogleSignIn";
 
 export default function LoginScreen() {
-  const [email, setEmail] = useState<string>("")
-  const [password, setPassword] = useState<string>("")
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const validateForm = useMemo(() => {
+    return () => {
+      if (!email.trim()) Alert.alert("Email is required");
+      else if (!/\S+@\S+\.\S+/.test(email)) Alert.alert("Invalid email format");
+
+      if (!password.trim()) Alert.alert("Password is required");
+      else if (password.length < 8)
+        Alert.alert("Password must at least be 8 characters long");
+
+      return true;
+    };
+  }, [email, password]);
 
   const handleSubmit = async () => {
+    if (!validateForm()) return;
+
     try {
-      await signInWithEmailAndPassword(auth, email, password)
-      Alert.alert(`User has signed in!, Welcome ${email}`)
+      await signInWithEmailAndPassword(auth, email, password);
+      Alert.alert(`User has signed in!, Welcome ${email}`);
     } catch (error) {
-      Alert.alert("User sign in has failed")
+      Alert.alert("User sign in has failed");
     }
-  }
+  };
 
   return (
-    <View>
-      <View> 
-        <Text>Email:</Text>
-        <TextInput placeholder="Email" value={email} onChangeText={(text) => setEmail(text)}/>
-      </View>
-      <View className="">
-        <Text>Password:</Text>
-        <TextInput placeholder="Password" value={password} onChangeText={(text) => setPassword(text)}/>
-      </View>
-      <Button title="Login" onPress={handleSubmit}/>
-      <View className="flex justify-center text-gray-700">
-        <Text>Or</Text>
-      </View>
-      <View>
-        <GoogleSignIn />
+    <View className="flex-1 items-center justify-center bg-slate-100">
+      <View className="w-full max-w-md px-5 py-8 bg-gray-100 rounded-lg shadow-md justify-center items-center">
+        <Text className="text-4xl mb-4">hILO</Text>
+        <View className="mb-4">
+          <Text className="mb-2">Enter your Email:</Text>
+          <TextInput
+            className="text-gray-900 border"
+            placeholder="Email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+        </View>
+        <View className="mb-4">
+          <Text className="mb-2">Enter your Password:</Text>
+          <TextInput
+            className="text-gray-900 border"
+            placeholder="Password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+        </View>
+        <TouchableOpacity onPress={handleSubmit}>
+          <Text className="bg-blue-500 text-white px-4 py-2 rounded mb-4">Login</Text>
+        </TouchableOpacity>
+        <View className="mb-4 justify-center items-center">
+          <Text className="text-gray-500">Or</Text>
+        </View>
+        <View className="mb-4">
+          <GoogleSignIn />
+        </View>
       </View>
     </View>
-  )
+  );
 }
