7535e122428301a51493d920056151b839c7572c
itchydimeee <124387580+itchydimeee@users.noreply.github.com>
2024-11-04 22:42:51 +0800

    [refactor] button

--- components/GoogleSignIn.tsx
+++ components/GoogleSignIn.tsx
@@ -1,17 +1,21 @@
-import React from "react";
-import { Button } from "react-native";
-import { signInWithCredential, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
-import * as WebBrowser from "expo-web-browser";
-import * as Google from "expo-auth-session/providers/google";
-import { auth } from "@/firebase/initializeFirebase";
-import { Routes } from "@/enums/routes";
-import { useRouter } from "expo-router";
-import uploadDocument from "@/helpers/firebase/uploadDocument";
-import queryDocument from "@/helpers/firebase/queryDocument";
+import React from 'react'
+import { Image, View, TouchableOpacity, Text } from 'react-native'
+import {
+  signInWithCredential,
+  GoogleAuthProvider,
+  onAuthStateChanged
+} from 'firebase/auth'
+import * as WebBrowser from 'expo-web-browser'
+import * as Google from 'expo-auth-session/providers/google'
+import { auth } from '@/firebase/initializeFirebase'
+import { Routes } from '@/enums/routes'
+import { useRouter } from 'expo-router'
+import uploadDocument from '@/helpers/firebase/uploadDocument'
+import queryDocument from '@/helpers/firebase/queryDocument'
 
-WebBrowser.maybeCompleteAuthSession();
+WebBrowser.maybeCompleteAuthSession()
 
-export default function GoogleSignIn() {
+export default function GoogleSignIn () {
   // const router = useRouter();
 
   // function handleNavigate() {
@@ -19,63 +23,67 @@ export default function GoogleSignIn() {
   // }
 
   const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-    clientId: process.env.EXPO_PUBLIC_CLIENT_ID,
-  });
+    clientId: process.env.EXPO_PUBLIC_CLIENT_ID
+  })
 
   React.useEffect(() => {
-    const unregistered = onAuthStateChanged(auth, async (user) => {
+    const unregistered = onAuthStateChanged(auth, async user => {
       if (user) {
-      
         const existingUsers = await queryDocument(
-          "User Account",
-          "uid",         
-          user.uid         
-        );
+          'User Account',
+          'uid',
+          user.uid
+        )
 
-        if (existingUsers.length === 0) { 
+        if (existingUsers.length === 0) {
           const userData = {
             uid: user.uid,
             name: user.displayName,
-            photo: user.photoURL,
-          };
-          uploadDocument("User Account", userData)
-            .then((docId) => {
-              console.log("User data uploaded successfully with ID:", docId);
+            photo: user.photoURL
+          }
+          uploadDocument('User Account', userData)
+            .then(docId => {
+              console.log('User data uploaded successfully with ID:', docId)
             })
-            .catch((error) => {
-              console.error("Error uploading user data:", error);
-            });
+            .catch(error => {
+              console.error('Error uploading user data:', error)
+            })
         } else {
-          console.log("User data already exists.");
+          console.log('User data already exists.')
         }
 
         // handleNavigate();
       }
-    });
-    return () => unregistered();
-  }, []);
+    })
+    return () => unregistered()
+  }, [])
 
   React.useEffect(() => {
-    if (response?.type === "success") {
-      const { id_token } = response.params;
-      const credential = GoogleAuthProvider.credential(id_token);
+    if (response?.type === 'success') {
+      const { id_token } = response.params
+      const credential = GoogleAuthProvider.credential(id_token)
       signInWithCredential(auth, credential)
-        .then((userCredential) => {
-          console.log("User signed in: ", userCredential.user);
+        .then(userCredential => {
+          console.log('User signed in: ', userCredential.user)
         })
-        .catch((error) => {
-          console.error("Google Sign-In Error: ", error);
-        });
+        .catch(error => {
+          console.error('Google Sign-In Error: ', error)
+        })
     }
-  }, [response]);
+  }, [response])
 
   return (
-    <Button
+    <TouchableOpacity
       disabled={!request}
-      title="Sign in with Google"
+      className='flex items-center' 
       onPress={() => {
         promptAsync();
       }}
-    />
+    >
+      <Image
+        source={require('../assets/images/google-logo.png')}
+        style={{ width: 200, height: 50 }}
+      />
+    </TouchableOpacity>
   );
 }
