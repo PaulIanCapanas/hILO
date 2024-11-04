c95ca1f893f683f8e8d2b74b5b8ef21b0442b1cd
itchydimeee <124387580+itchydimeee@users.noreply.github.com>
2024-11-04 14:58:32 +0800

    [add] checker for existing emails

--- components/GoogleSignIn.tsx
+++ components/GoogleSignIn.tsx
@@ -1,29 +1,67 @@
 import React from "react";
 import { Button } from "react-native";
-import { signInWithCredential, GoogleAuthProvider } from "firebase/auth";
+import { signInWithCredential, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
 import { auth } from "@/firebase/initializeFirebase";
-
+import { Routes } from "@/enums/routes";
+import { useRouter } from "expo-router";
+import uploadDocument from "@/helpers/firebase/uploadDocument";
+import queryDocument from "@/helpers/firebase/queryDocument";
 
 WebBrowser.maybeCompleteAuthSession();
 
 export default function GoogleSignIn() {
+  // const router = useRouter();
+
+  // function handleNavigate() {
+  //   router.push(Routes.USERPROFILE);
+  // }
+
   const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
     clientId: process.env.EXPO_PUBLIC_CLIENT_ID,
   });
 
   React.useEffect(() => {
+    const unregistered = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+      
+        const existingUsers = await queryDocument(
+          "User Account",
+          "uid",         
+          user.uid         
+        );
+
+        if (existingUsers.length === 0) { 
+          const userData = {
+            uid: user.uid,
+            name: user.displayName,
+            photo: user.photoURL,
+          };
+          uploadDocument("User Account", userData)
+            .then((docId) => {
+              console.log("User data uploaded successfully with ID:", docId);
+            })
+            .catch((error) => {
+              console.error("Error uploading user data:", error);
+            });
+        } else {
+          console.log("User data already exists.");
+        }
+
+        // handleNavigate();
+      }
+    });
+    return () => unregistered();
+  }, []);
+
+  React.useEffect(() => {
     if (response?.type === "success") {
       const { id_token } = response.params;
       const credential = GoogleAuthProvider.credential(id_token);
-      signInWithCredential(auth, credential)
-        .then((userCredential) => {
-          console.log("User signed in: ", userCredential.user);
-        })
-        .catch((error) => {
-          console.error("Google Sign-In Error: ", error);
-        });
+      signInWithCredential(auth, credential).catch((error) => {
+        console.error("Google Sign-In Error: ", error);
+      });
     }
   }, [response]);
 
