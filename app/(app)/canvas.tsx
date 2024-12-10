a9bd9793502f0b8baa12ef558bd6f8e0a1499bd8
itchydimeee <124387580+itchydimeee@users.noreply.github.com>
2024-12-10 17:11:25 +0800

    [Add] added upload design data to firestore
    1. Created a component that will upload the design data to firestore
    2. Fetch the title, category, and date data to canvas page

--- app/(app)/canvas.tsx
+++ app/(app)/canvas.tsx
@@ -1,35 +1,73 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native'
 import { useLocalSearchParams } from 'expo-router'
+import queryDocument from '@/helpers/firebase/queryDocument'
+import { Collection } from '@/enums/collections'
 
-export default function CanvasPage() {
-  const { title, description, selectedDesignType } = useLocalSearchParams<{
+export default function CanvasPage () {
+  const { key, title, description, selectedDesignType } = useLocalSearchParams<{
+    key: string
     title: string
     description: string
     selectedDesignType: string
-  }>();
+  }>()
+  const [designData, setDesignData] = useState<any>(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchDesignData = async () => {
+      setLoading(true)
+      try {
+        const data = await queryDocument(Collection.DESIGNS, 'key', key)
+        if (data.length > 0) {
+          setDesignData(data[0])
+        }
+      } catch (error) {
+        console.error('Error fetching design data:', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchDesignData()
+  }, [key])
+
+  if (loading) {
+    return (
+      <View
+        className='flex-1 items-center justify-center p-4'
+        testID='loading-indicator'
+      >
+        <ActivityIndicator size='large' color='#65558F' />
+      </View>
+    )
+  }
+
+  function updatedAtToString () {
+    const date = designData.updatedAt
+    const dateString = date.toDate().toDateString()
+    return dateString
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Canvas Page</Text>
-      <Text style={styles.label}>Title: {title}</Text>
-      <Text style={styles.label}>Description: {description}</Text>
-      <Text style={styles.label}>Design Type: {selectedDesignType}</Text>
+      <Text style={styles.label}>{title}</Text>
+      <Text style={styles.label}>{selectedDesignType}</Text>
+      <Text style={styles.label}>{updatedAtToString()}</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingTop: 40,
   },
   label: {
     fontSize: 18,
     marginBottom: 10,
+    textAlign: 'center',
   },
 })
--- app/(app)/createDesign.tsx
+++ app/(app)/createDesign.tsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, TextInput, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native'
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  ImageBackground,
+} from 'react-native'
 import { Picker } from '@react-native-picker/picker'
 import { useLocalSearchParams, useRouter } from 'expo-router'
-import { Routes } from '@/enums/routes'
-
+import UploadDesign from '@/components/UploadDesign'
 
 export default function CreateNewDesign () {
   const { designType } = useLocalSearchParams<{
@@ -15,60 +20,54 @@ export default function CreateNewDesign () {
   const router = useRouter()
 
   useEffect(() => {
-    setSelectedDesignType(designType);
-  }, [designType]);
+    setSelectedDesignType(designType)
+  }, [designType])
 
-  function navToCanvasPage () {
-    router.push({
-        pathname: Routes.CANVAS,
-        params: { title, description, selectedDesignType }
-    })
-  }
-
-   const header = '../../assets/images/header.jpg'
+  const header = '../../assets/images/header.jpg'
 
   return (
     <ImageBackground
-    source={require(header)}
-    style={{
-      flex: 1,
-      width: '100%',
-      justifyContent: 'center',
-    }}
-  >
-    <View style={styles.container}>
-      <Text style={styles.header}>Create New Design</Text>
-      <View style={styles.table}>
-      <TextInput
-        style={styles.input}
-        placeholder='Untitled'
-        value={title}
-        onChangeText={setTitle}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder='Description'
-        value={description}
-        onChangeText={setDescription}
-        multiline
-      />
-      <View style={styles.picker}>
-      <Picker
-        selectedValue={selectedDesignType}
-        onValueChange={itemValue => setSelectedDesignType(itemValue)}
-        
-      >
-        <Picker.Item label='Plain' value='Plain' />
-        <Picker.Item label='Pattern' value='Pattern' />
-        <Picker.Item label='Checkered' value='Checkered' />
-        <Picker.Item label='Others' value='Others' />
-      </Picker>
+      source={require(header)}
+      style={{
+        flex: 1,
+        width: '100%',
+        justifyContent: 'center',
+      }}
+    >
+      <View style={styles.container}>
+        <Text style={styles.header}>Create New Design</Text>
+        <View style={styles.table}>
+          <TextInput
+            style={styles.input}
+            placeholder='Untitled'
+            value={title}
+            onChangeText={setTitle}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='Description'
+            value={description}
+            onChangeText={setDescription}
+            multiline
+          />
+          <View style={styles.picker}>
+            <Picker
+              selectedValue={selectedDesignType}
+              onValueChange={itemValue => setSelectedDesignType(itemValue)}
+            >
+              <Picker.Item label='Plain' value='Plain' />
+              <Picker.Item label='Pattern' value='Pattern' />
+              <Picker.Item label='Checkered' value='Checkered' />
+              <Picker.Item label='Others' value='Others' />
+            </Picker>
+          </View>
+          <UploadDesign
+            title={title}
+            description={description}
+            selectedDesignType={selectedDesignType}
+          />
+        </View>
       </View>
-      <TouchableOpacity onPress={navToCanvasPage} style={{padding: 10, alignItems: 'center', justifyContent: 'center',}}>
-        <Text style={{backgroundColor: '#D7C9ED', borderRadius: 10, paddingHorizontal: 20, paddingVertical: 10}}>Create</Text>
-      </TouchableOpacity>
-      </View>
-    </View>
     </ImageBackground>
   )
 }
@@ -86,7 +85,7 @@ const styles = StyleSheet.create({
   table: {
     backgroundColor: '#ffffff',
     padding: 20,
-    borderRadius: 10
+    borderRadius: 10,
   },
   input: {
     height: 40,
@@ -100,6 +99,6 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     borderColor: 'gray',
     borderWidth: 1,
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
 })
