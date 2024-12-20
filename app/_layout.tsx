6c517dacbfb0afd72d07e4232a5dd6d436cc883f
cezro <hyderz1218@gmail.com>
2024-12-20 12:45:41 +0800

    [Fix] removed home from signin and login layout stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,17 +3,17 @@ import {
   DefaultTheme,
   ThemeProvider,
   NavigationContainer,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-import "react-native-reanimated";
-import "../global.css";
+} from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+import '../global.css';
 
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { Routes } from "@/enums/routes";
-import { SessionProvider } from "@/contexts/AuthContext";
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { Routes } from '@/enums/routes';
+import { SessionProvider } from '@/contexts/AuthContext';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -21,7 +21,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
   useEffect(() => {
@@ -36,13 +36,18 @@ export default function RootLayout() {
 
   return (
     <SessionProvider>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <NavigationContainer>
           <Stack>
             <Stack.Screen name="(app)" options={{ headerShown: false }} />
-            <Stack.Screen name={Routes.LOGIN} options={{ headerShown: false }} />
-            <Stack.Screen name={Routes.SIGNUP} options={{ headerShown: false }} />
-            <Stack.Screen name={Routes.HOME} options={{ headerShown: false }} />
+            <Stack.Screen
+              name={Routes.LOGIN}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name={Routes.SIGNUP}
+              options={{ headerShown: false }}
+            />
           </Stack>
         </NavigationContainer>
       </ThemeProvider>
